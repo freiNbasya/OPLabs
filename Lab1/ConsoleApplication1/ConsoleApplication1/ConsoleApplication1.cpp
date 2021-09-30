027fb0bd4d3c674803b4cba29dc1c342c75bd3c1
pr1koldes <90349154+pr1koldes@users.noreply.github.com>
2021-09-30 11:15:58 +0300

    Add files via upload

---
+++ Lab1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a, d, n;
+	cin >> a;// вводимо номер першого члену
+	cin >> d; // вводимо різницю
+	cin >> n; // вводимо номер n-ого члену
+	cout << a + (n - 1) * d;
+	return 0;
+}
