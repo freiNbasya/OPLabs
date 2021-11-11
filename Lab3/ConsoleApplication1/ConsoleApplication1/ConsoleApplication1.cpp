d8a67e555f252fe94524a5fe88368ea130f15fa4
pr1koldes <90349154+pr1koldes@users.noreply.github.com>
2021-11-11 12:23:04 +0200

    Add files via upload

---
+++ Lab3/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int factorial(int j)
+{
+    if (j == 0) return 1;
+    else return j * factorial(j - 1);
+}
+int main()
+{
+    int j;
+    int x;
+    int S;
+    int a;
+    int a_pred;
+    int i;
+    int n;
+    
+    cin >> x;
+    if (x >= 0 && x <= 5) {
+        n = 0;
+        i = 2 * n;
+        a = 1;
+        a_pred = a;
+        S = a;
+            while (abs(a - a_pred) < 0.0001)
+            {
+               
+                n = n + 1;
+                i = 2 * n;
+                a_pred = a;
+                a = pow(-1, n) * pow(x, 2 * n) / factorial(i);
+                if (n % 2 == 0) {
+                    S = S + a;
+                }
+            }
+            cout << S;
+    }
+    else { cout << ("X не належить проміжку"); }
+
+}
+
