8c92a740e6bec309e101eb1e87c4b7dee23f51c1
pr1koldes <90349154+pr1koldes@users.noreply.github.com>
2021-11-11 12:24:02 +0200

    Add files via upload

---
+++ Lab4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float x;
+	int n;
+	float y;
+	float yPred;
+	float a;
+
+	cin >> x;
+	cin >> a;
+	cin >> n;
+	if (x > 0 && n >= 0 && a > 0) {
+		y = a;
+		
+		for (int i = 0; i < n; i++) {
+			yPred = y;
+			y = 0.5 * (yPred + (x / yPred));
+		}
+			cout << y << endl;
+	}
+		
+}
+
+
