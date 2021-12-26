054c7878d139fce318f91717dcf9cf988945e77e
pr1koldes <90349154+pr1koldes@users.noreply.github.com>
2021-12-26 19:56:20 +0200

    Add files via upload

---
+++ Lab8/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int** bud_mass(int n){
+    int** mass = new int* [n];
+    for (int i = 0; i < n; ++i) {
+        mass[i] = new int[n];
+        for (int j = 0; j < n; ++j) {
+            mass[i][j] = (i + j) % n + 1;
+        }
+    }
+    return mass;
+}
+
+int main() {
+    int const n = 6;
+   
+    int** mass = bud_mass(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << mass[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+ 
+}
